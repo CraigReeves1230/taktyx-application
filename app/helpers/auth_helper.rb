8b94e460c95460ef31756761ce7e813476f15ed4
Christopher Reeves <ChrisReeves12@yahoo.com>
2016-06-14 22:15:51 -0400

    fixed bug in auth helper

--- app/helpers/auth_helper.rb
+++ app/helpers/auth_helper.rb
@@ -4,7 +4,7 @@ module AuthHelper
   def auth_user
     # Get the currently logged in user
     if session.has_key? :user_id
-      @auth_user ||= User.find(session[:user_id])
+      @auth_user ||= User.find_by_id(session[:user_id])
     end
   end
 end
