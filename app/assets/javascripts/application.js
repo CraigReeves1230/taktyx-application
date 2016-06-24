cdf0127e601634e9bebbd54f8759da569c2afda8
Christopher Reeves <ChrisReeves12@yahoo.com>
2016-06-23 20:24:06 -0400

    ditched asset pipeline

--- app/assets/javascripts/application.js
+++
@@ -1,9 +0,0 @@
-// Load dependencies for all of site
-
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap
-//= require angular
-//= require globals.js.erb
-//= require controllers
-//= require_tree .
