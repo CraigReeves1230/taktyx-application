e9b024844bbb2be5686438cc69ec17a42e781f2c
CraigReeves1230 <reevescd1@gmail.com>
2016-07-08 02:00:40 -0400

    switched rails out to 5.0.0rc1

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 source 'https://rubygems.org'
 
 
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.2.5'
+# Bundle Rails 5 racecar
+gem 'rails', '~> 5.0.0.rc1'
 
 # Use mysql as the database for Active Record
 gem 'mysql2', '>= 0.3.13', '< 0.5'
--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ apt-get install -y curl
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
 source /usr/local/rvm/scripts/rvm
-rvm install 2.2.4
+rvm reinstall 2.3.0
 gem install bundler
 apt-get install -y git
 apt-get install -y cmake
